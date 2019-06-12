8d712c2397c3b79d7e95db809e5102c239f61f94
Amina <painrealmm@gmail.com>
2019-06-12 22:40:05 +0100

    Updates

---
+++ precache-manifest.0850153bdd89cbdc12f4a895a15d7e9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "44a12aa642d94d9ed198",
+    "url": "/react-gh-pages/static/js/main.44a12aa6.chunk.js"
+  },
+  {
+    "revision": "949864524584e39c5b2e",
+    "url": "/react-gh-pages/static/js/2.94986452.chunk.js"
+  },
+  {
+    "revision": "44a12aa642d94d9ed198",
+    "url": "/react-gh-pages/static/css/main.23f49af5.chunk.css"
+  },
+  {
+    "revision": "a574372a9e4a1c8795d790fd8018e2c7",
+    "url": "/react-gh-pages/index.html"
+  }
+];
